4a6718c92eabd0ce2b436729ab3385934eb87fb0
DiscoDoggy <akaattarilim@gmail.com>
2023-09-09 15:37:57 -0700

    Added more User Agents and randomized the selection of User Agents in request

--- data_collect.py
+++ data_collect.py
@@ -6,6 +6,8 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import requests
 
+import random
+
 """
 This file collects the raw shakespeare plays and the translated
 Shakespeare plays
@@ -16,8 +18,15 @@ load_dotenv('secrets.env')
 
 #Function defines a more realistic header for the scraper
 def define_request_headers():
+    
+    user_agents = [
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/117.0",\
+        "Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36",\
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9",\
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"]
+
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/117.0",
+        "User-Agent": random.choice(user_agents),
         "Accept-Encoding" : "gzip, deflate, br",
         "Accept-Language" : "en-US,en;q=0.5",
         "Connection" : "keep-alive",
@@ -32,6 +41,8 @@ def scrape_shakespeare_page(URL):
     headers = define_request_headers()
     response = requests.get(URL, headers=headers)
 
+    
+
     if response.status_code == 200:
         shakespeare_html = response.content
     else:
@@ -40,11 +51,14 @@ def scrape_shakespeare_page(URL):
     
     return shakespeare_html
 
-    #r = requests.get("https://httpbin.org/headers", headers=headers)
-    #print(r.text)
+#For testing User-Agent
+# r = requests.get("https://httpbin.org/headers", headers=headers)
+# print(r.text)
+
+    
 
 #function calls scrape_shakespeare_page, extracts the HTML
-#and parses HTML to obtain a link to each play which is stored and returned in a list
+#and parses HTML to obtain a link to each plays' acts which is stored and returned in a list
 def get_shakespeare_play_links():
     URL = os.environ['TRANSLATIONS_HOMEPAGE']
     DOMAIN_NAME = os.environ['DOMAIN_NAME']
@@ -57,9 +71,14 @@ def get_shakespeare_play_links():
     for play in soup.find_all("a", class_="translation hoverable"):
 
         play_link = DOMAIN_NAME + play.get('href')
-        print(type(play_link))
         link_to_play_acts.append(play_link)
+        print(play_link)
  
-    print(link_to_play_acts)
+    return link_to_play_acts
 
-    
+def scrape_acts_links():
+    link_to_play_acts = get_shakespeare_play_links()
+
+    #for each play,
+        #get the links to the content of the act
+        # append to a list     
