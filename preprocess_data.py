dfd933d3ae6526b074090c90e9ee585d9ef0ca45
DiscoDoggy <akaattarilim@gmail.com>
2023-09-23 23:26:06 -0700

    Added 3 spaces to single space to cleaning function

--- preprocess_data.py
+++ preprocess_data.py
@@ -5,13 +5,17 @@ import re
 def clean_unk_char_ws(data):
     """
     * Purpose: replaces the � character with empty string and 
-    * gets rid of trailing and leading whitespace
+        * Gets rid of trailing and leading whitespace
+        * When reading in from CSV, littered with segments that have 3 spaces
+        * Adjusted these 3 spaces to be a single space
 
     Parameters: Pandas Dataframe
     Returns: Cleaned Pandas Dataframe 
     """
     data = data.map(lambda text : re.sub('�',"", str(text)))
-    
+
+    data = data.map(lambda text : re.sub('   '," ", str(text)))
+
     data = data.map(lambda text : str(text).lstrip())
     data = data.map(lambda text : str(text).rstrip())
 
@@ -21,4 +25,8 @@ df = pd.read_csv('shakespeare_and_translation_original_data.csv')
 df = clean_unk_char_ws(df)
 
 for i in range(20):
-    print('hi'+df['Untranslated Shakespeare'][i])
+    print('hi'+df['Translated Shakespeare'][i])
+# for char in df['Untranslated Shakespeare'][4]:
+#     print(ord(char))
+
+
