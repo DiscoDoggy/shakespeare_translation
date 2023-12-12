dbcfaa49f5e9d102f94db3b60a0acb1b501940bf
DiscoDoggy <akaattarilim@gmail.com>
2023-12-12 13:56:30 -0800

    Finished New Preprocessing *added in building and application of transforms *Partitioning data into (X1,X2,...,Xn) and (y1,y2,...,yn) from ((X1,Y1), ...,(Xn,yn)) *Bucket batched the data *Created some methods to print what the data looks like

--- newPreprocess.py
+++ newPreprocess.py
@@ -3,7 +3,7 @@ import torch
 import torchtext.transforms
 from torchtext.transforms import VocabTransform
 import torchdata.datapipes as dp
-from torchdata.datapipes.iter import IterDataPipe
+from torchdata.datapipes.iter import IterableWrapper
 from torchtext.vocab import build_vocab_from_iterator
 
 def preprocess_main():
@@ -13,8 +13,10 @@ def preprocess_main():
     data_pipe = dp.iter.FileOpener(data_pipe, mode='rb')
     data_pipe = data_pipe.parse_csv(skip_lines=0, delimiter=',', as_tuple = True)
 
-    print_data_pipe_sample(data_pipe, 30)
+    print_data_pipe_sample(data_pipe,10)
 
+    global mod_eng_vocab
+    global old_eng_vocab
     mod_eng_vocab = build_vocabulary(data_pipe, 0)
     old_eng_vocab = build_vocabulary(data_pipe, 1)
 
@@ -22,11 +24,28 @@ def preprocess_main():
     print(old_eng_vocab.get_itos()[:9])
 
     print("\nTESTING TRANSFORMS:\n")
-    for i in range(30):
+    for i in range(10):
         print_transforms_sample(data_pipe, mod_eng_vocab, 0, i)
         print_transforms_sample(data_pipe, old_eng_vocab, 1, i)
         print('\n')
 
+    data_pipe = data_pipe.map(apply_transforms)
+    # temp = list(data_pipe)
+    # print(temp[:10])
+
+    data_pipe = data_pipe.bucketbatch(
+        batch_size=4, batch_num=5, bucket_num=1,
+        use_in_batch_shuffle=False, sort_key = sort_bucket
+    )
+
+    data_pipe = data_pipe.map(separate_src_tgt)
+    print(list(data_pipe)[1])
+
+    data_pipe = data_pipe.map(apply_padding)
+
+    print('\n')
+
+    print_fully_processed_samples(data_pipe, mod_eng_vocab, old_eng_vocab, 5)
 
 def tokenize(txt_segment):
     segment = txt_segment
@@ -71,6 +90,34 @@ def build_transforms(vocab):
 
     return text_transform
 
+def apply_transforms(txt_seg_pair):
+    return(
+        build_transforms(mod_eng_vocab)(tokenize(txt_seg_pair[0])),
+        build_transforms(old_eng_vocab)(tokenize(txt_seg_pair[1]))
+    )
+
+def sort_bucket(bucket):
+    return sorted(bucket, key=lambda x: (len(x[0]), len(x[1])))
+
+def separate_src_tgt(lang_pairs):
+    """
+    *Datapipe formats data as ((X1,Y1),...,(Xn,yn))
+    *Want data to be formatted as (X1,X2,X3,...,Xn) and (y1,y2,...,yn)
+    *and to be aligned
+    """
+    sources,targets = zip(*lang_pairs)
+    return sources, targets
+
+def apply_padding(sequence_pair):
+    """
+    *Converts each index in tuple to a tensor and adds padding
+    *torchtext's ToTensor automatically adds padding and the 0
+    *represents the index of the pad token in the overall vocabulary
+    """
+    return (torchtext.transforms.ToTensor(0)(list(sequence_pair[0])), 
+            torchtext.transforms.ToTensor(0)(list(sequence_pair[1]))
+    )
+
 def print_data_pipe_sample(data_pipe, num_samples):
     cnt = 0
 
@@ -92,6 +139,24 @@ def print_transforms_sample(datapipe, vocab, place, sentence_idx):
     index_to_string = vocab.get_itos()
     for index in transformed_sentence:
         print(index_to_string[index], end=' ')
-        
+
+def print_fully_processed_samples(data_pipe, mod_eng_lex, old_eng_lex, num_samples):
+    mod_eng_idx_to_str = mod_eng_lex.get_itos()
+    old_eng_idx_to_str = old_eng_lex.get_itos()
+
+    for srcs, tgts in data_pipe:
+        if srcs[0][-1] != 0:
+            continue
+        for i in range(4):
+            source = ""
+            for token in srcs[i]:
+                source += " " + mod_eng_idx_to_str[token]
+            target = ""
+            for token in tgts[i]:
+                target += " " + old_eng_idx_to_str[token]
+            print(f"Source: {source}")
+            print(f"Target: {target}\n")
+        break
+
 
 preprocess_main()
