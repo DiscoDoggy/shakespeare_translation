a5df1f40ee89cf30b4819f42bd4975ce57bbbbbb
DiscoDoggy <akaattarilim@gmail.com>
2024-01-07 13:50:36 -0800

    Updated gitignore for discord token env and created a basic hello world for the discord bot that conected to a server

--- discord_bot/shakespearean_discord_bot.py
+++ discord_bot/shakespearean_discord_bot.py
@@ -1,2 +1,29 @@
 #what we want is to grab text from the user and send that text to inference.py
 # where it then sends translated text back
+#https://realpython.com/how-to-make-a-discord-bot-python/ 
+
+import os
+import discord
+from dotenv.main import load_dotenv
+
+load_dotenv('discord_bot\secrets.env')
+TOKEN = os.environ["DISCORD_TOKEN"]
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+print(TOKEN)
+
+@client.event
+async def on_ready():
+    print(f"{client.user}")
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith("Shi ass bot"):
+        await message.channel.send("You a shi ass human")
+
+client.run(TOKEN)
